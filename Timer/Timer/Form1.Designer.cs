e950f3e31a8b1f49532473cb19fc9ae7249da23d
Danya Kovinskiy <129722708+Danilo666228@users.noreply.github.com>
2024-01-17 20:37:08 +0300

    Add files via upload

---
+++ Timer/Timer/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnSbros = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Lime;
+            this.label1.Font = new System.Drawing.Font("Cascadia Code", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(502, 127);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00:00:00";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cascadia Code", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.OliveDrab;
+            this.label2.Location = new System.Drawing.Point(179, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Дата";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Lime;
+            this.label3.Font = new System.Drawing.Font("Cascadia Code", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Yellow;
+            this.label3.Location = new System.Drawing.Point(146, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(280, 106);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "0:0.0";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Cascadia Code", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnStop.Location = new System.Drawing.Point(145, 292);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Cтоп";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnSbros
+            // 
+            this.btnSbros.Font = new System.Drawing.Font("Cascadia Code", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSbros.Location = new System.Drawing.Point(351, 292);
+            this.btnSbros.Name = "btnSbros";
+            this.btnSbros.Size = new System.Drawing.Size(75, 23);
+            this.btnSbros.TabIndex = 4;
+            this.btnSbros.Text = "Сброс";
+            this.btnSbros.UseVisualStyleBackColor = true;
+            this.btnSbros.Click += new System.EventHandler(this.btnSbros_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 325);
+            this.Controls.Add(this.btnSbros);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnSbros;
+    }
+}
+
---
+++ Timer/Timer/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+        private int m, s, ms;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+                btnStop.Text = "Старт";
+            }
+            else
+            {
+                timer1.Start();
+                btnStop.Text = "Стоп";
+            }
+        }
+
+        private void btnSbros_Click(object sender, EventArgs e)
+        {
+            m = 0; s = 0; ms = 0;
+            label3.Text = "0:0.0";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToLongTimeString();
+            label2.Text = DateTime.Now.ToLongDateString();
+            ms++;
+            s = ms / 10;
+            m = s / 60;
+            label3.Text = m + ":" + s % 60 + "." + ms % 10;
+        }
+    }
+}
